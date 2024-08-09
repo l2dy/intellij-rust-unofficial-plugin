c96d3e6102d2830037a4ff42f7df900916712bd6
Zero King <l2dy@icloud.com>
2024-08-09 21:46:47 +0800

    Update IntelliJ Platform Gradle Plugin to 2.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,7 +46,7 @@ val basePluginArchiveName = "intellij-rust"
 plugins {
     idea
     kotlin("jvm") version "2.0.10"
-    id("org.jetbrains.intellij.platform") version "2.0.0"
+    id("org.jetbrains.intellij.platform") version "2.0.1"
     id("org.jetbrains.grammarkit") version "2022.3.2.2"
     id("net.saliman.properties") version "1.5.2"
     id("org.gradle.test-retry") version "1.5.10"
