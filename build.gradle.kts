cf52586215fe893506dae4b2f2ff6763b9ca5ae3
Zero King <l2dy@icloud.com>
2025-09-26 21:17:27 +0800

    GRD: Fix OutOfMemoryError in tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -223,7 +223,7 @@ allprojects {
         }
 
         tasks.withType<Test>().configureEach {
-            jvmArgs = listOf("-Xmx3g", "-XX:-OmitStackTraceInFastThrow")
+            jvmArgs = listOf("-Xmx5g", "-XX:-OmitStackTraceInFastThrow")
 
             // We need to prevent the platform-specific shared JNA library to loading from the system library paths,
             // because otherwise it can lead to compatibility issues.
