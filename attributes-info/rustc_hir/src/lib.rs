9e4baa977e6dc43268287bdc9e15adc1a8249c05
Zero King <l2dy@icloud.com>
2025-10-20 01:39:14 +0800

    Update attributes-info to rust commit 70e26c1b7bd

--- attributes-info/rustc_hir/src/lib.rs
+++ attributes-info/rustc_hir/src/lib.rs
@@ -1,3 +1,9 @@
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+pub enum AttrStyle {
+    Outer,
+    Inner,
+}
+
 pub mod attrs {
     #[derive(Copy, Clone, PartialEq, Eq)]
     pub enum EncodeCrossCrate {
