da739b6ad2560409caf083ade9ceee1f72825754
Zero King <l2dy@icloud.com>
2025-10-20 02:00:23 +0800

    Update attributes-info to rust commit a5c9030271d

--- attributes-info/rustc_feature/build.rs
+++ attributes-info/rustc_feature/build.rs
@@ -23,7 +23,43 @@ fn download_file<T: IntoUrl>(url: T, file_name: &str) {
         .collect::<Vec<_>>()
         .join("\n");
 
+    let text = if file_name == "unstable.rs" {
+        adjust_unstable_file(text)
+    } else {
+        text
+    };
+
     let out_dir = env::var("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join(file_name);
     fs::write(dest_path, &text).unwrap();
+}
+
+fn adjust_unstable_file(text: String) -> String {
+    const OLD_BODY: &str = r#"        self.enabled_lang_features
+            .iter()
+            .map(|feat| (feat.gate_name, feat.attr_sp))
+            .chain(self.enabled_lib_features.iter().map(|feat| (feat.gate_name, feat.attr_sp)))
+    }
+"#;
+    const NEW_BODY: &str = r#"        let mut enabled_features: Vec<_> = self
+            .enabled_lang_features
+            .iter()
+            .map(|feat| (feat.gate_name, feat.attr_sp))
+            .collect();
+        enabled_features.extend(
+            self.enabled_lib_features
+                .iter()
+                .map(|feat| (feat.gate_name, feat.attr_sp)),
+        );
+        enabled_features.into_iter()
+    }
+"#;
+
+    if let Some(index) = text.find(OLD_BODY) {
+        let mut patched = text.clone();
+        patched.replace_range(index..index + OLD_BODY.len(), NEW_BODY);
+        patched
+    } else {
+        panic!("failed to adjust unstable.rs: expected iterator body not found");
+    }
 }
